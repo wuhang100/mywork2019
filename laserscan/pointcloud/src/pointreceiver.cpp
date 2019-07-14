029a8e9a8cc636cd320b79928b55ca89632cc4d0
wuhang100 <36547153+wuhang100@users.noreply.github.com>
2019-07-14 23:42:56 +0800

    Add files via upload

--- laserscan/pointcloud/src/pointreceiver.cpp
+++ laserscan/pointcloud/src/pointreceiver.cpp
@@ -10,11 +10,9 @@ class PointCluster
 {
 public:
     PointCluster(){
-    ROS_INFO("Handle the point cloud");
-    cloud_send.header.frame_id = std::string("base_laser");
-    pub = nh.advertise<sensor_msgs::PointCloud2>("pointcluster", 10);
-    sub = nh.subscribe("pointmessage", 10, &PointCluster::cloudconcentrate, this);
-    
+        ROS_INFO("Handle the point cloud");
+        pub = nh.advertise<sensor_msgs::PointCloud2>("pointcluster", 10);
+        sub = nh.subscribe("pointmessage", 10, &PointCluster::cloudconcentrate, this);        
     }
  
     void cloudconcentrate (const sensor_msgs::PointCloud2ConstPtr& cloud_msg){
@@ -22,17 +20,20 @@ public:
     //pcl::PCLPointCloud2 cloud2;
     //pcl_conversions::toPCL(*cloud_msg,cloud2);
     //pcl::fromPCLPointCloud2(cloud2,*cloud);
-    pcl::fromROSMsg(*cloud_msg,cloud);
-    cloud_con +=  cloud;
-    ROS_INFO("The size is %lud", cloud_con.points.size ());
-    pcl::toROSMsg(cloud_con,cloud_send);
-    cloud_send.header.frame_id = std::string("base_laser");
-    //std::cout << cloud_send.header.frame_id << std::endl;
-    //listener.waitForTransform("base_laser","base_link",\
-                              ros::Time(0),\
-                              ros::Duration(1.0));
-    cloud_send.header.stamp = ros::Time(0);
-    pub.publish(cloud_send);
+        pcl::fromROSMsg(*cloud_msg,cloud);
+        cloud_con +=  cloud;
+        if (cloud_con.points.size () < 10000){
+            ROS_INFO("The size is %lud", cloud_con.points.size ());
+        }
+        else {
+            ROS_INFO("The size is %lud", cloud_con.points.size ());
+            pcl::toROSMsg(cloud_con,cloud_send);
+            cloud_send.header.stamp = ros::Time(0);
+            cloud_send.header.frame_id = std::string("base_link");
+            ROS_INFO("Sending cloud...");
+            pub.publish(cloud_send);
+            cloud_con = cloud_con_empty;
+    }
     }
  
 private:
@@ -43,6 +44,7 @@ private:
     ros::NodeHandle nh;
     tf::TransformListener listener;
     pcl::PointCloud<pcl::PointXYZ> cloud_con;
+    pcl::PointCloud<pcl::PointXYZ> cloud_con_empty;
 };
  
 int main(int argc, char **argv){
